393e57506248ad656172d05313a98442f6039a20
procyon4 <git@chriswinkler.dev>
2020-01-16 12:18:53 -0800

    Created initial app and pages

---
+++ my-site/src/pages/ArticlesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlesList = () => {
+    return (
+        <div>
+            <h1>Articles List Page!</h1>
+        </div>
+    )
+}
+
+export default ArticlesList
---
+++ my-site/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>
+            This is a bunch of random placeholder text!
+            I'm just gonna blab here about my D&D campaign since it's on my mind.
+            The players will be fighting a Warlock alongside the Octarran Alliance.
+            They will 
+        </p>
+    </>
+)
+
+export default HomePage;
